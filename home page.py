e5803c8e27e63386e7a1d550280f74ddc76d8446
programmer-atieb <atieb.nji@gmail.com>
2025-08-31 17:52:46 +0300

    Update home page.py

--- home page.py
+++ home page.py
@@ -14,150 +14,152 @@ if st.sidebar.button("FAQs"):
     st.session_state.page = "FAQs"
 
 Israeli_products = [
-    ADAMA
-AHAVA
-AXA
-Achva
-Actavis Generics
-Activision Blizzard
-Adidas
-Ahava
-Airbnb
-Amazon
-Apple
-BNP Paribas
-BP
-Bankinvest
-Barak
-Barclays
-Booking.com
-Burger King
-Carrefour
-Caterpillar
-Check Point
-Check Point Software Technologies
-Chevron
-Cisco
-Citibank
-Coca Cola
-Crazy Labs
-CyberArk
-Dan Hotels
-Danske Bank
-Dell
-Deutsche Bank
-Disney
-Domino's Pizza
-EA Games
-Eden Springs
-Elite
-Expedia
-ExxonMobil
-Facebook
-Fiverr
-Ford
-G4S
-General Electric
-General Motors
-Gilat Satellite Networks
-Goldman Sachs
-Google
-H&M
-HP
-HSBC
-Hyundai
-IBM
-IKEA
-Imperva
-Indigo
-Instagram
-Intel
-JPMorgan Chase
-Johnson & Johnson
-KFC
-Keter
-L'Oréal
-LG
-Lumenis
-Lyft
-Mazda
-McAfee
-McDonalds
-Micron
-Microsoft
-Minecraft
-Moderna
-Monday.com
-Moon Active
-Morgan Stanley
-NVIDIA
-NaanDan
-Nestlé
-Netafim
-Netflix
-Neto Malinda Trading
-Nike
-Nykredit
-Oracle
-Ormat
-Osem
-PayPal
-Pepsi
-Pfizer
-Plarium
-Plastro
-Playtika
-Prigat
-Procter & Gamble
-Puma
-PureGym
-Qualcomm
-RAD Data Communications
-Radware
-Ratiopharm
-Reebok
-Rivulis
-Russia
-Sabra
-Sabra Hummus
-Samsung
-Shell
-Siemens
-SodaStream
-Sony
-Starbucks
-Strauss Group
-Tabnine
-Tara
-Telrad Networks
-Tempo
-Tempur Sealy
-Tesco Bank
-Tesla
-Teva
-Teva Pharmaceutical Industries
-Texaco
-Tivall
-Tnuva
-Toyota
-Trip.com
-TripAdvisor
-Tui
-Twitter / X
-Uber
-Unilever
-Varonis
-Volkswagen
-Volvo
-Volvo (AB Volvo trucks)
-WaterGen
-Wendy's
-WhatsApp
-Wix
-ZARA
-ZARA (owned by Inditex)
-Zim Integrated Shipping Services
-eBay
+companies = [
+    "ADAMA",
+    "AHAVA",
+    "AXA",
+    "Achva",
+    "Actavis Generics",
+    "Activision Blizzard",
+    "Adidas",
+    "Airbnb",
+    "Amazon",
+    "Apple",
+    "BNP Paribas",
+    "BP",
+    "Bankinvest",
+    "Barak",
+    "Barclays",
+    "Booking.com",
+    "Burger King",
+    "Carrefour",
+    "Caterpillar",
+    "Check Point",
+    "Check Point Software Technologies",
+    "Chevron",
+    "Cisco",
+    "Citibank",
+    "Coca Cola",
+    "Crazy Labs",
+    "CyberArk",
+    "Dan Hotels",
+    "Danske Bank",
+    "Dell",
+    "Deutsche Bank",
+    "Disney",
+    "Domino's Pizza",
+    "EA Games",
+    "Eden Springs",
+    "Elite",
+    "Expedia",
+    "ExxonMobil",
+    "Facebook",
+    "Fiverr",
+    "Ford",
+    "G4S",
+    "General Electric",
+    "General Motors",
+    "Gilat Satellite Networks",
+    "Goldman Sachs",
+    "Google",
+    "H&M",
+    "HP",
+    "HSBC",
+    "Hyundai",
+    "IBM",
+    "IKEA",
+    "Imperva",
+    "Indigo",
+    "Instagram",
+    "Intel",
+    "JPMorgan Chase",
+    "Johnson & Johnson",
+    "KFC",
+    "Keter",
+    "L'Oréal",
+    "LG",
+    "Lumenis",
+    "Lyft",
+    "Mazda",
+    "McAfee",
+    "McDonalds",
+    "Micron",
+    "Microsoft",
+    "Minecraft",
+    "Moderna",
+    "Monday.com",
+    "Moon Active",
+    "Morgan Stanley",
+    "NVIDIA",
+    "NaanDan",
+    "Nestlé",
+    "Netafim",
+    "Netflix",
+    "Neto Malinda Trading",
+    "Nike",
+    "Nykredit",
+    "Oracle",
+    "Ormat",
+    "Osem",
+    "PayPal",
+    "Pepsi",
+    "Pfizer",
+    "Plarium",
+    "Plastro",
+    "Playtika",
+    "Prigat",
+    "Procter & Gamble",
+    "Puma",
+    "PureGym",
+    "Qualcomm",
+    "RAD Data Communications",
+    "Radware",
+    "Ratiopharm",
+    "Reebok",
+    "Rivulis",
+    "Russia",
+    "Sabra",
+    "Sabra Hummus",
+    "Samsung",
+    "Shell",
+    "Siemens",
+    "SodaStream",
+    "Sony",
+    "Starbucks",
+    "Strauss Group",
+    "Tabnine",
+    "Tara",
+    "Telrad Networks",
+    "Tempo",
+    "Tempur Sealy",
+    "Tesco Bank",
+    "Tesla",
+    "Teva",
+    "Teva Pharmaceutical Industries",
+    "Texaco",
+    "Tivall",
+    "Tnuva",
+    "Toyota",
+    "Trip.com",
+    "TripAdvisor",
+    "Tui",
+    "Twitter / X",
+    "Uber",
+    "Unilever",
+    "Varonis",
+    "Volkswagen",
+    "Volvo",
+    "Volvo (AB Volvo trucks)",
+    "WaterGen",
+    "Wendy's",
+    "WhatsApp",
+    "Wix",
+    "ZARA",
+    "ZARA (owned by Inditex)",
+    "Zim Integrated Shipping Services",
+    "eBay"
+]
+
 
 
 if st.session_state.page == "Homepage":
